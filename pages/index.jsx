27c437cb9a1f74a44ede1b12d47c23e95eeddfd9
dante01yoon <bunggl@naver.com>
2021-09-19 13:22:29 +0900

    Update: catch all routes

---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Page = () => <div>IndexPage</div>
+
+export default Page;
---
+++ pages/notes/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Notes = () => <div>Notes Page</div>
+
+export default Notes;
