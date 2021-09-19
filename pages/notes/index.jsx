47ac0674efba7d0add0f70f937a62256f104fb67
dante01yoon <bunggl@naver.com>
2021-09-19 17:20:37 +0900

    Update notes.module.css

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "../../src/styles/notes.module.css";
 
 const Notes = () => {
   const router = useRouter();
@@ -10,7 +11,7 @@ const Notes = () => {
     <div>
       <h1> Note</h1>
       <Link href="/notes/[id]" as={`/notes/1`}>note 1</Link>
-      <button onClick={() => router.push("/notes/[id]", "/notes/2")}>note 2</button>
+      <button className={styles.notes_button} onClick={() => router.push("/notes/[id]", "/notes/2")}>note 2</button>
     </div>
   )
 }
